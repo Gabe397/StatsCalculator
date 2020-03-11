463552783881db5a2637d21af857fb46ee0355e4
gabe3 <Dinya7991>
2020-03-11 14:17:26 -0400

    Changed Test for Median to randomly generated

--- test/median.test.js
+++ test/median.test.js
@@ -1,5 +1,9 @@
 const meanStats = require('../medianStats');
+const ss = require('simple-statistics');
+const randomList = require('../RLGSeed');
 
 test('gets mean of 4, 1, 7 to equal 4', () => {
-    expect(meanStats.median([4,1,7])).toBe(4);
+    let array1 = randomList.generateListSeed(0,90,0, "hello", 10);
+    let testFunction = ss.median(array1);
+    expect(meanStats.median(array1)).toBe(testFunction);
 });
